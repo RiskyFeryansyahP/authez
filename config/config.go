4c29f79cdc8739966db2a499f8f1215c149f21c8
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-07-03 15:20:48 +0700

    add new config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,19 @@
 package config
+
+// ConfigMap is configuration option for authentication
+type ConfigMap struct {
+	DB *Database
+}
+
+// NewConfigMap return a configmap with a default value in table name (`users`)
+func NewConfigMap() *ConfigMap {
+	return &ConfigMap{
+		DB: &Database{
+			DBHost:     "",
+			DBPassword: "",
+			DBUsername: "",
+			DBName:     "",
+			TableName:  "users",
+		},
+	}
+}
