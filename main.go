5ebe33af374f8ecae31ad6d05c9322d3c0b8b686
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-07-03 15:20:23 +0700

    add new main

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"github.com/confus1on/authez/internal/service/auth/handler"
+	"github.com/confus1on/authez/internal/service/auth/repository"
+	"github.com/confus1on/authez/internal/service/auth/usecase"
+	fasthttprouter "github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
+	"log"
+	"os"
+)
+
+func main()  {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	port = ":" + port
+
+	router := fasthttprouter.New()
+
+	authRepo := repository.NewAuthRepository()
+	authUsecase := usecase.NewAuthUsecase(authRepo)
+	handler.NewAuthHandler(authUsecase, router)
+
+	log.Printf("Server running at 127.0.0.1%s", port)
+	log.Fatal(fasthttp.ListenAndServe(port, router.Handler))
+}
