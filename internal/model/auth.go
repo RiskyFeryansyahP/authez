f64c40274902b618cbe3a61effe0be212519134c
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-07-11 11:57:57 +0700

    delete unused field

--- internal/model/auth.go
+++ internal/model/auth.go
@@ -1,10 +1,8 @@
 package model
 
-import "github.com/confus1on/authez/config"
-
 // InputAuth is input config for authentication
 type InputAuth struct {
-	DB       config.Database `json:"database"`
-	Username string          `json:"username"`
-	Password string          `json:"password"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	TableName string `json:"table_name"`
 }
