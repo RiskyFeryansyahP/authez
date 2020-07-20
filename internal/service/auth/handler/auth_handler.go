656e59ffa30716501dedc95faaf6b318981fa4ed
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-07-20 11:25:12 +0700

    add google handler and google signin

--- internal/service/auth/handler/auth_handler.go
+++ internal/service/auth/handler/auth_handler.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
+	"encoding/base64"
 	"encoding/json"
+	"log"
+	"math/rand"
+	"time"
 
+	"github.com/confus1on/authez/config"
 	"github.com/confus1on/authez/internal/model"
 	"github.com/confus1on/authez/internal/service/auth"
 	fasthttprouter "github.com/fasthttp/router"
@@ -19,6 +24,8 @@ func NewAuthHandler(authUC auth.UsecaseAuth, router *fasthttprouter.Router) {
 	authHandler := &AuthHandler{AuthUC: authUC}
 
 	router.POST("/v1/signin", authHandler.Signin)
+	router.GET("/auth/google/callback", authHandler.HandleGoogleSignin)
+	router.GET("/v1/signin/google", signinGoogle)
 }
 
 // Signin http method
@@ -42,4 +49,59 @@ func (a *AuthHandler) Signin(ctx *fasthttp.RequestCtx) {
 	}
 
 	_ = json.NewEncoder(ctx).Encode(result)
+}
+
+// HandleGoogleSignin handle request google signin
+func (a *AuthHandler) HandleGoogleSignin(ctx *fasthttp.RequestCtx) {
+	byteCode := ctx.FormValue("code")
+	byteState := ctx.FormValue("state")
+
+	code := string(byteCode)
+	state := string(byteState)
+
+	log.Println("state", state)
+
+	configMap := config.NewConfigMap()
+
+	ctx.Response.Header.SetContentType("application/json")
+
+	response, err := a.AuthUC.GoogleAuthentication(configMap, state, code)
+	if err != nil {
+		ctx.Response.Header.SetStatusCode(fasthttp.StatusBadRequest)
+		_ = json.NewEncoder(ctx).Encode(&model.ResponseError{
+			Code:    fasthttp.StatusBadRequest,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	_ = json.NewEncoder(ctx).Encode(response)
+}
+
+func signinGoogle(ctx *fasthttp.RequestCtx) {
+	conf := config.NewConfigMap()
+
+	url := conf.GoogleOauth.AuthCodeURL("oauthstate")
+
+	ctx.Redirect(url, fasthttp.StatusTemporaryRedirect)
+}
+
+func generateStateAuthCookie(ctx *fasthttp.RequestCtx) string {
+	expiration := time.Now().Add(365 * 24 * time.Hour)
+
+	b := make([]byte, 16)
+
+	_, _ = rand.Read(b)
+
+	state := base64.URLEncoding.EncodeToString(b)
+
+	cookie := &fasthttp.Cookie{}
+	cookie.SetKey("oauthstate")
+	cookie.SetValue(state)
+	cookie.SetExpire(expiration)
+	cookie.SetHTTPOnly(true)
+
+	ctx.Response.Header.SetCookie(cookie)
+
+	return state
 }
