8b5cabc71f6333ca9c254051d3d0d05896132527
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-07-20 11:24:37 +0700

    add google authentication usecase

--- internal/service/auth/usecase.go
+++ internal/service/auth/usecase.go
@@ -1,11 +1,12 @@
 package auth
 
 import (
+	"github.com/confus1on/authez/config"
 	"github.com/confus1on/authez/internal/model"
 )
 
 // UsecaseAuth is abstract will be use in usecase package
 type UsecaseAuth interface {
 	AuthenticationValidation(input model.InputAuth) (interface{}, error)
-	GoogleAuthentication() (interface{}, error)
+	GoogleAuthentication(config *config.ConfigMap, state, code string) (interface{}, error)
 }
